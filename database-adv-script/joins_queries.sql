3255495486f8724662da7251b27478444139ca66
rreda11 <141592396+rreda11@users.noreply.github.com>
2025-05-13 15:20:57 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,17 +1,18 @@
---Write a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+--a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
 SELECT *
 FROM Booking b
 INNER JOIN [User] u ON b.user_id = u.user_id;
 
 
 
--- Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+-- a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 SELECT *
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 
---Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+--a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT *
 FROM [User] u
 FULL OUTER JOIN Booking b ON u.user_id = b.user_id;

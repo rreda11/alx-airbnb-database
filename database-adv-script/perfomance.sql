2fa8243ba9492b4ee0d988abc8284cde61e7f442
rreda11 <141592396+rreda11@users.noreply.github.com>
2025-05-13 16:55:32 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -24,3 +24,6 @@ FROM Booking b
 INNER JOIN [User] u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+WHERE b.status NOT IN ('cancelled', 'deleted')
+AND b.end_date >= CURRENT_DATE
+AND u.role = 'customer';
